ab20b741a1dda7a9139177399a8618f8f07ecb65
saville <saville@adobe>
2019-12-12 10:11:48 -0700

    Separate pagerduty integration from plugins

--- slackbot/plugins/oncall.py
+++ slackbot/plugins/oncall.py
@@ -1,15 +1,11 @@
 
 import logging
 import re
-from datetime import datetime
-from pytz import timezone, UTC
 from typing import Optional
 from pygerduty.v2 import ContactMethod, User
-from pygerduty.v2 import PagerDuty
 from slackbot.bot import listen_to
-from slackbot import settings
 from slackbot.dispatcher import Message
-import humanize
+from slackbot import pagerduty
 
 
 LOGGER = logging.getLogger(__name__)
@@ -22,16 +18,6 @@ CONTACT_METHOD_EMOTICON_BY_TYPE = {
 }
 
 
-def _is_pagerduty_configured() -> bool:
-    return settings.PAGERDUTY_TOKEN and settings.PAGERDUTY_SCHEDULE_ID
-
-
-def _get_client() -> Optional[PagerDuty]:
-    if not _is_pagerduty_configured():
-        return None
-    return PagerDuty(settings.PAGERDUTY_TOKEN)
-
-
 def _get_contact_method_message(contact_method: ContactMethod, add_label: bool) -> Optional[str]:
     emoticon = CONTACT_METHOD_EMOTICON_BY_TYPE.get(contact_method.type)
     if not emoticon or (hasattr(contact_method, 'enabled') and not contact_method.enabled):
@@ -42,44 +28,20 @@ def _get_contact_method_message(contact_method: ContactMethod, add_label: bool) 
     return f'{emoticon} {contact_method.address}{label}'
 
 
-def _get_user_name(user: User) -> Optional[str]:
-    if not settings.PAGERDUTY_USERNAME_EMAIL_DOMAIN:
-        return None
-    email_suffix = f'@{settings.PAGERDUTY_USERNAME_EMAIL_DOMAIN}'
-    if not user.email.endswith(email_suffix):
-        return None
-    return user.email.replace(email_suffix, '')
-
-
-def _get_formatted_datetime(dest_time_zone_str: str, datetime_string: str) -> str:
-    dest_time_zone = timezone(dest_time_zone_str)
-    parsed = datetime.strptime(datetime_string, '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=UTC)
-    parsed = parsed.replace(tzinfo=dest_time_zone)
-    result = humanize.naturaldate(parsed)
-    if result != 'today':
-        return result
-    return humanize.naturaltime(parsed - datetime.now(dest_time_zone))
-
-
 @listen_to(r'who is (?:currently )?on(?:-| |)call', re.IGNORECASE)
 def who_is_on_call(message: Message):
-    client = _get_client()
-    if not client:
-        LOGGER.debug('Pager duty settings are not configured, ignoring on call request')
+    if not pagerduty.is_configured():
+        LOGGER.debug('Pager duty settings are not configured, cannot retrieve current on call')
         return
 
-    oncalls = client.oncalls.list(include=['users'], schedule_ids=[settings.PAGERDUTY_SCHEDULE_ID])
-    current_oncall = None
-    for oncall in oncalls:
-        current_oncall = oncall
-        break
+    current_oncall = pagerduty.get_current_oncall()
     if not current_oncall:
-        message.reply(f'No current on-call information found for schedule {settings.PAGERDUTY_SCHEDULE_ID}')
+        message.reply(f'No current on-call information found for schedule {pagerduty.get_schedule_id()}')
         return
 
     # Build up contact methods string
     contact_method_messages = []
-    user_name = _get_user_name(current_oncall.user)
+    user_name = pagerduty.get_user_name(current_oncall.user)
     if user_name:
         contact_method_messages.append(f':slack: @{user_name}')
     contact_methods = current_oncall.user.contact_methods.list(time_zone='MDT')
@@ -90,7 +52,7 @@ def who_is_on_call(message: Message):
             contact_method_messages.append(contact_method_message)
 
     # Get formatted end time
-    end_time = _get_formatted_datetime(current_oncall.user.time_zone, current_oncall.end)
+    end_time = pagerduty.get_humanized_datetime(current_oncall.end, current_oncall.user.time_zone)
 
     message.reply_webapi(f'{current_oncall.user.name} is currently on call', in_thread=True, blocks=[
         {
